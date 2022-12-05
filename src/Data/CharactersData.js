904fe07ab2feefc9a2594ea86797c82661e4c889
NathanWigen <nathan.s.wigen@hotmail.com>
2022-12-05 13:14:08 -0800

    added pictures and styling
    added pictures to imgur added mobile styling to character page

--- src/Data/CharactersData.js
+++ src/Data/CharactersData.js
@@ -1,47 +1,47 @@
 const characterData = [
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/310429277_166563842629473_1600284793846943792_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=otG77t0J5vMAX8hu0pn&_nc_ht=scontent-sea1-1.xx&oh=00_AfAc8QcEx4n0QhX2xBgmv1tPhpPFVFxcgTGPsfLnAHEO3w&oe=638056B5",
+      "https://i.imgur.com/R80igcf.jpg",
     alt: "Snow Queen",
     name: "Snow Queen",
   },
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },
   {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },  {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },  {
     src:
-      "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/273972264_101869589100825_676534382089333055_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zk1p6hLOWbAAX_bjVhA&_nc_ht=scontent-sea1-1.xx&oh=00_AfDQpSAvvrTfpNhWY8gToV92W8fKoD8gXSdyLiSw-KH2KQ&oe=63804409",
+      "https://i.imgur.com/VS8y1pK.jpg",
       alt: "Snow Princess",
       name: "Snow Princess",
   },
